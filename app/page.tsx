8335f68eb22a76e32adbe7ea2f4ddf550f3eb125
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-23 07:56:27 +0800

    Installing all possibly required Shadcn UI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import { Button } from '@/components/ui/button'
 function HomePage() {
-  return <h1>Homepage</h1>
+  return (
+    <div>
+      <h1 className='text-3xl'>Homepage</h1>
+      <Button
+        variant='outline'
+        size='lg'
+        className='capitalize m-8'
+      >
+        add
+      </Button>
+    </div>
+  )
 }
 
 export default HomePage
