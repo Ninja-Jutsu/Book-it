0c1213463c672ed771217bae7b5364e5994785c9
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-24 02:42:28 +0800

    Setup Scadcn theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-
 const inter = Inter({ subsets: ['latin'] })
+// compo
+import Navbar from '@/components/navbar/Navbar'
+import Providers from './providers'
 
 export const metadata: Metadata = {
   title: 'Book-it',
@@ -17,8 +19,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
+    <html
+      lang='en'
+      suppressHydrationWarning
+    >
+      <body className={inter.className}>
+        <Providers>
+          <Navbar />
+          <main className='container py-10'>{children}</main>
+        </Providers>
+      </body>
     </html>
   )
 }
