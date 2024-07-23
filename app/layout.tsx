46909c5d4e099eac2826c79b9ea1b09164ae56b3
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-24 02:23:09 +0800

    Refactor main layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+<<<<<<< HEAD
 
+=======
+>>>>>>> 92f950e (Refactor main layout file)
 
 //components
 import Navbar from '@/components/navbar/Navbar'
