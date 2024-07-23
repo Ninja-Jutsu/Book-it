a404a2084c5ea569b732ce596bcc434bad9f0f1c
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-24 06:27:55 +0800

    Create first user profile & update his clerk Metadata

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,15 +1,40 @@
 'use server'
-
+import prisma from '@/prisma/client'
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server'
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
 import { profileSchema } from './schemas'
 
 export async function createProfileAction(prevState: any, formData: FormData) {
   try {
+    // get the currentUser
+    const user = await currentUser()
+    // if (!user) throw new Error('please log to create a profile')
+    // this is for typescript or use ! before user
+    // this is a protected route, there is definitely a user if we get here
+
+    // extract form data
     const rawData = Object.fromEntries(formData)
     const validatedFields = profileSchema.parse(rawData)
-    console.log(validatedFields)
-    return { message: 'Profile created' }
+
+    // create a profile
+    await prisma.profile.create({
+      data: {
+        clerkId: user!.id,
+        email: user!.emailAddresses[0].emailAddress,
+        profileImage: user!.imageUrl ?? '',
+        ...validatedFields,
+      },
+    })
+    await clerkClient.users.updateUserMetadata(user!.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    })
+    // return { message: 'Profile created' }
   } catch (error) {
     console.log(error)
-    return { message: 'error?.errors.message' }
+    return { message: error instanceof Error ? error.message : 'An error occurred' }
   }
+  redirect('/')
 }
