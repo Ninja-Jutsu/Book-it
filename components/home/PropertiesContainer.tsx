36d601b168f01c6d8706215ab0c4978cf934ef6b
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-25 06:34:57 +0800

    Setup the initial Properties Container compo

--- components/home/PropertiesContainer.tsx
+++ components/home/PropertiesContainer.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { fetchProperties } from '@/utils/actions'
+import PropertiesList from './PropertiesList'
+import EmptyList from './EmptyList'
+import type { PropertyCardProps } from '@/utils/types'
 
 type Props = {
   category?: string
   search?: string
 }
 
-function PropertiesContainer({ category, search }: Props) {
-  return <div></div>
+async function PropertiesContainer({ category, search }: Props) {
+  const properties: PropertyCardProps[] = await fetchProperties({ category, search })
+
+  if (properties.length === 0) {
+    return (
+      <EmptyList
+        heading='No result.'
+        message='Try changing or removing filters'
+        btnText='Clear Filter'
+      />
+    )
+  }
+
+  return <PropertiesList />
 }
 
 export default PropertiesContainer
