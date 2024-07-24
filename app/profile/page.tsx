16a07c64a2d7aa3910d0445d5ae0a9146cd4c89f
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-24 09:29:02 +0800

    Setup Profile page with user data

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import FormContainer from '@/components/form/FormContainer'
+import { updateProfileAction, fetchProfileAction } from '@/utils/actions'
+import FormInput from '@/components/form/FormInput'
+import { SubmitButton } from '@/components/form/Buttons'
 
-function ProfilePage() {
-  return <h1 className='text-3xl'>ProfilePage</h1>
+async function ProfilePage() {
+  const profile = await fetchProfileAction()
+
+  return (
+    <section>
+      <h1 className='text-2xl font-semibold mb-8 capitalize'>user profile</h1>
+      <div className='border p-8 rounded-md'>
+        {/* image input container */}
+
+        <FormContainer action={updateProfileAction}>
+          <div className='grid gap-4 md:grid-cols-2 mt-4 '>
+            <FormInput
+              inputType='text'
+              inputId='firstName'
+              label='First Name'
+              defaultValue={profile.firstName}
+            />
+            <FormInput
+              inputType='text'
+              inputId='lastName'
+              label='Last Name'
+              defaultValue={profile.lastName}
+            />
+            <FormInput
+              inputType='text'
+              inputId='username'
+              label='Username'
+              defaultValue={profile.username}
+            />
+          </div>
+          <SubmitButton
+            text='Update Profile'
+            className='mt-8'
+          />
+        </FormContainer>
+      </div>
+    </section>
+  )
 }
 
 export default ProfilePage
