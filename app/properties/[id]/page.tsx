be156a70656e5b19998ebbb6cde2c52fa4d45eba
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-26 05:11:57 +0800

    Build ImageContainer & add it to PropertyDetailsPage

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,7 @@
-
 import FavoriteToggleBtn from '@/components/card/FavoriteToggleBtn'
+import PropertyRating from '@/components/card/PropertyRating'
 import BreadCrumbs from '@/components/properties/BreadCrumbs'
+import ImageContainer from '@/components/properties/ImageContainer'
 import ShareButton from '@/components/properties/ShareButton'
 import { fetchPropertyDetails } from '@/utils/actions'
 import { redirect } from 'next/navigation'
@@ -11,7 +12,7 @@ async function PropertyDetailsPage({ params: { id } }: { params: { id: string } 
 
   if (!property) redirect('/')
 
-  const { id: propertyId, baths, bedrooms, beds, guests, tagline, name } = property
+  const { id: propertyId, baths, bedrooms, beds, guests, tagline, name, image } = property
   const details = { baths, bedrooms, beds, guests }
   return (
     <section>
@@ -27,6 +28,22 @@ async function PropertyDetailsPage({ params: { id } }: { params: { id: string } 
           <FavoriteToggleBtn propertyId={propertyId} />
         </div>
       </header>
+      <ImageContainer
+        name={name}
+        mainImage={image}
+      />
+      <section className='lg:grid lg:grid-cols-12 gap-x-12 mt-12'>
+        <div className='lg:col-span-8'>
+          <div className='flex gap-4 items-center'>
+            <h1 className='text-xl font-bold'>{name}</h1>
+            <PropertyRating
+              propertyId={propertyId}
+              inPage={true}
+            />
+          </div>
+        </div>
+        <div className='lg:col-span-4 flex flex-col items-center'>{/* calender */}</div>
+      </section>
     </section>
   )
 }
