0df479673051be8415a595b2578b2c8e2bb84456
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-26 02:04:03 +0800

    Apply NavSearch functionalities

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,10 +1,46 @@
+'use client'
 import { Input } from '../ui/input'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useDebouncedCallback } from 'use-debounce'
+import { useState, useEffect } from 'react'
 function NavSearch() {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+  const searchParam = searchParams.get('search')
+
+  const [search, setSearch] = useState(searchParam?.toString() || '')
+
+  // we don't want this func to run on every key stroke: we use debounce to set a time-frame
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams)
+    if (value) {
+      params.set('search', value)
+    } else {
+      params.delete('search')
+    }
+    // replace to the home page with the other params untouched
+    replace(`${pathname}?${params.toString()}`)
+  }, 300)
+
+  // Every time searchParam has no value, remove it:
+  useEffect(() => {
+    if (!searchParam) {
+      setSearch('')
+    }
+  }, [searchParam])
+
   return (
     <Input
       type='text'
       placeholder='find a property'
       className='max-w-xs dark:bg-muted'
+      value={search}
+      onChange={(e) => {
+        const value = e.target.value
+        setSearch(value)
+        handleSearch(value)
+      }}
     />
   )
 }
