5603e3e2bdd47e21bdc84be05602d48685e5fe80
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-26 03:15:05 +0800

    Build FavoriteToggleBtn

--- components/card/FavoriteToggleBtn.tsx
+++ components/card/FavoriteToggleBtn.tsx
@@ -2,18 +2,19 @@ import { FaHeart } from 'react-icons/fa'
 import { Button } from '@/components/ui/button'
 import { auth } from '@clerk/nextjs/server'
 import { CardSignInButton } from '../form/Buttons'
+import { fetchFavoriteId } from '@/utils/actions'
+import FavoriteToggleForm from './FavoriteToggleForm'
 
-function FavoriteToggleBtn({ propertyId }: { propertyId: string }) {
+async function FavoriteToggleBtn({ propertyId }: { propertyId: string }) {
   const { userId } = auth()
   if (!userId) return <CardSignInButton />
+
+  const favoriteId = await fetchFavoriteId({ propertyId })
   return (
-    <Button
-      size='icon'
-      variant='outline'
-      className='p-2 cursor-pointer'
-    >
-      <FaHeart />
-    </Button>
+    <FavoriteToggleForm
+      favoriteId={favoriteId}
+      propertyId={propertyId}
+    />
   )
 }
 export default FavoriteToggleBtn
