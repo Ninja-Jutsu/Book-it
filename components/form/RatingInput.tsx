4a2797aaa2148e1afef8a0dcc926943af6bdc1a9
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-26 09:59:54 +0800

    Create RatingInput component

---
+++ components/form/RatingInput.tsx
@@ -0,0 +1,44 @@
+import { Label } from '@/components/ui/label'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+
+//   const numbers = Array.from({ length: 5 }, (_, i) => {
+//     const value = i + 1
+//     return value.toString()
+//   }).reverse()
+const numbers = ['5', '4', '3', '2', '1']
+
+function RatingInput({ name, labelText }: { name: string; labelText?: string }) {
+  return (
+    <div className='mb-2 max-w-xs'>
+      <Label
+        htmlFor={name}
+        className='capitalize'
+      >
+        {labelText || name}
+      </Label>
+      <Select
+        defaultValue={numbers[0]}
+        name={name}
+        required
+      >
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {numbers.map((num) => {
+            return (
+              <SelectItem
+                key={num}
+                value={num}
+              >
+                {num}
+              </SelectItem>
+            )
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  )
+}
+
+export default RatingInput
