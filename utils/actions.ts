dc510828385ceca4e0fdf116dfa1bda590d09bb4
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-27 07:54:38 +0800

    Finalize updatePropertyAction & updatePropertyImageAction

--- utils/actions.ts
+++ utils/actions.ts
@@ -509,10 +509,51 @@ export const fetchRentalDetails = async (propertyId: string) => {
   })
 }
 
-export const updatePropertyAction = async () => {
-  return { message: 'update property action' }
+export const updatePropertyAction = async (prevState: any, formData: FormData): Promise<{ message: string }> => {
+  const user = await getCurrentUser()
+  const propertyId = formData.get('id') as string
+
+  try {
+    const rawData = Object.fromEntries(formData)
+    const validatedFields = validateWithZodSchema(propertySchema, rawData)
+    await prisma.property.update({
+      where: {
+        id: propertyId,
+        profileId: user.id,
+      },
+      data: {
+        ...validatedFields,
+      },
+    })
+
+    revalidatePath(`/rentals/${propertyId}/edit`)
+    return { message: 'Update Successful' }
+  } catch (error) {
+    return renderError(error)
+  }
 }
 
-export const updatePropertyImageAction = async () => {
-  return { message: 'update property image' }
+export const updatePropertyImageAction = async (prevState: any, formData: FormData): Promise<{ message: string }> => {
+  const user = await getCurrentUser()
+  const propertyId = formData.get('id') as string
+
+  try {
+    const image = formData.get('image') as File
+    const validatedFields = validateWithZodSchema(imageSchema, { image })
+    const fullPath = await uploadImage(validatedFields.image)
+
+    await prisma.property.update({
+      where: {
+        id: propertyId,
+        profileId: user.id,
+      },
+      data: {
+        image: fullPath,
+      },
+    })
+    revalidatePath(`/rentals/${propertyId}/edit`)
+    return { message: 'Property Image Updated Successful' }
+  } catch (error) {
+    return renderError(error)
+  }
 }
