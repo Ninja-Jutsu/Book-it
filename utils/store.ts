6b6a2084fe5447210f5731ade325b6faceff2c41
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-27 03:14:52 +0800

    Setup Zustand library and create initial state shape

---
+++ utils/store.ts
@@ -0,0 +1,22 @@
+import { create } from 'zustand'
+import { Booking } from './types'
+import { DateRange } from 'react-day-picker'
+// Define the state's shape
+type PropertyState = {
+  propertyId: string
+  price: number
+  bookings: Booking[]
+  range: DateRange | undefined
+}
+
+// Create the store
+export function useProperty() {
+  create<PropertyState>(() => {
+    return {
+      propertyId: '',
+      price: 0,
+      bookings: [],
+      range: undefined,
+    }
+  })
+}
