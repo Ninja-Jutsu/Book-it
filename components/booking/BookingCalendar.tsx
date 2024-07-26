cba97f0fcc809bd56fc04b137ba135140a23cfd9
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-27 03:39:43 +0800

    Setup BookingCalendar Date Selection functionality

--- components/booking/BookingCalendar.tsx
+++ components/booking/BookingCalendar.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+'use client'
+import { Calendar } from '@/components/ui/calendar'
+import { useEffect, useState } from 'react'
+import { useToast } from '@/components/ui/use-toast'
+import { DateRange } from 'react-day-picker'
+import { useProperty } from '@/utils/store'
+
+import { generateDisabledDates, generateDateRange, defaultSelected, generateBlockedPeriods } from '@/utils/calendar'
 
 function BookingCalendar() {
+  const currentDate = new Date()
+  const [range, setRange] = useState<DateRange | undefined>(defaultSelected)
+
+  // every time we select a range => update the range in the Zustand state
+  useEffect(() => {
+    useProperty.setState({
+      range,
+    })
+  }, [range])
   return (
-    <div>
-      
-    </div>
+    <Calendar
+      mode='range'
+      defaultMonth={currentDate}
+      selected={range}
+      onSelect={setRange}
+      className='mb-4'
+    />
   )
 }
 
--- components/booking/BookingContainer.tsx
+++ components/booking/BookingContainer.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+'use client'
+import { useProperty } from '@/utils/store'
+import ConfirmBooking from './ConfirmBooking'
+import BookingForm from './BookingForm'
 
 function BookingContainer() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const state = useProperty((state) => state)
+  console.log(state)
+  return <div className='w-full'>
+    <BookingForm />
+    <ConfirmBooking />
+  </div>
 }
 
 export default BookingContainer
