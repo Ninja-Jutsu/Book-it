ce92f419f1e42fc67ec59271a47bb2f31469cbb3
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-26 09:59:43 +0800

    Write needed form actions for reviews

--- utils/actions.ts
+++ utils/actions.ts
@@ -243,3 +243,19 @@ export async function fetchPropertyDetails(id: string) {
     },
   })
 }
+
+export const createReviewAction = async () => {
+  return { message: 'create review' }
+}
+
+export const fetchPropertyReviews = async () => {
+  return { message: 'fetch reviews' }
+}
+
+export const fetchPropertyReviewsByUser = async () => {
+  return { message: 'fetch user reviews' }
+}
+
+export const deleteReviewAction = async () => {
+  return { message: 'delete  reviews' }
+}
