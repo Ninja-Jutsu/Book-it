fd0d164538ad36b6478c9917825918df78b446b9
ismail-bardach <ismail.bardach.work@gmail.com>
2024-07-27 06:00:54 +0800

    Build booking page

--- app/bookings/page.tsx
+++ app/bookings/page.tsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import EmptyList from '@/components/home/EmptyList'
+import CountryFlagAndName from '@/components/card/CountryFlagAndName'
+import Link from 'next/link'
 
-function BookingsPage() {
+import { formatDate, formatCurrency } from '@/utils/format'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
+
+import FormContainer from '@/components/form/FormContainer'
+import { IconButton } from '@/components/form/Buttons'
+import { fetchBookings, deleteBookingAction } from '@/utils/actions'
+
+async function BookingsPage() {
+  const bookings = await fetchBookings()
+  if (bookings.length === 0) return <EmptyList heading='You have no reservations yet!' btnText='Back to Reservations' />
   return (
-    <div>
-      <h1 className='text-3xl'>BookingsPage</h1>;
+    <div className='mt-16'>
+      <h4 className='font-bold text-xl mb-4 capitalize'>total bookings : {bookings.length}</h4>
+      <Table>
+        <TableCaption>A list of your recent bookings</TableCaption>
+
+        <TableHeader>
+          <TableRow>
+            <TableHead>Property Name</TableHead>
+            <TableHead>Country</TableHead>
+            <TableHead>Nights</TableHead>
+            <TableHead>Total</TableHead>
+            <TableHead>Check In</TableHead>
+            <TableHead>Check Out</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {bookings.map(({ id, orderTotal, totalNights, checkIn, checkOut, property }) => {
+            const { id: propertyId, name: propertyName, country } = property
+            const startDate = formatDate(checkIn)
+            const endDate = formatDate(checkOut)
+            return (
+              <TableRow key={id}>
+                <TableCell>
+                  <Link
+                    href={`/properties/${propertyId}`}
+                    className='underline text-muted-foreground tracking-wide'
+                  >
+                    {propertyName}
+                  </Link>
+                </TableCell>
+                <TableCell>
+                  <CountryFlagAndName countryCode={country} />
+                </TableCell>
+                <TableCell>{totalNights}</TableCell>
+                <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                <TableCell>{startDate}</TableCell>
+                <TableCell>{endDate}</TableCell>
+                <TableCell>
+                  <DeleteBooking bookingId={id} />
+                </TableCell>
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
     </div>
+  )
+}
+
+function DeleteBooking({ bookingId }: { bookingId: string }) {
+  const deleteBooking = deleteBookingAction.bind(null, { bookingId })
+  return (
+    <FormContainer action={deleteBooking}>
+      <IconButton actionType='delete' />
+    </FormContainer>
   )
 }
 
--- components/form/FormContainer.tsx
+++ components/form/FormContainer.tsx
@@ -4,7 +4,6 @@ import { useFormState } from 'react-dom'
 import { useEffect } from 'react'
 import { useToast } from '@/components/ui/use-toast'
 import { actionFunction } from '@/utils/types'
-// import { actionFunction } from '@/utils/types'
 
 const initialState = {
   message: '',
